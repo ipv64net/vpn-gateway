bb93163f66f25e0db72be90617f7f529c75d02c8
CalthaPalustris <calthapalustris@outlook.com>
2025-07-17 20:27:03 +0000

    consistent output

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -3,17 +3,17 @@
 set -e
 
 if [ -z "$WG_TOKEN" ]; then
-    echo "ERROR: WG_TOKEN is not set."
+    echo "[ERROR] Missing WG_TOKEN"
     exit 1
 fi
 
 WG_URL="https://ipv64.net?wgconfig=${WG_TOKEN}"
-echo "[INFO] Downloading WireGuard configuration from: $WG_URL"
 
+echo "[INFO] Downloading WireGuard configuration from: $WG_URL"
 mkdir -p /etc/wireguard
 curl -fsSL "$WG_URL" -o /etc/wireguard/wg0.conf
 
-echo "[INFO] Starting WireGuard interface wg0..."
+echo "[INFO] Starting WireGuard interface wg0"
 wg-quick up wg0
 
 # Halte den Container am Leben
